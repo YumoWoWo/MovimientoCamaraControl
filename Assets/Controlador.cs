1267e400fb3f479ddccb5c6e4be2e09bccc79977
YumoWoWo <95858638+YumoWoWo@users.noreply.github.com>
2022-09-30 13:54:08 +0200

    MovimientCamaraControlCamera

--- Assets/Controlador.cs
+++ Assets/Controlador.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Controlador : MonoBehaviour
 {
     private CharacterController controller;
+    public Transform cam;
     private Vector3 playerVelocity;
     
     public Transform groundSensor;
@@ -14,27 +15,58 @@ public class Controlador : MonoBehaviour
     public float speed = 5;
     public float jumpForce = 20f;
     public float jumpHeight = 1f;
-    private float gravity = -9.81f;
+    private float gravity = -12f;
     public bool isGrounded;
+
+    private float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
     
     void Awake()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
+    {
+       // Movement();
+       MovementTPS();
+        
+    Jump();
+       
+    }
+
+    void Movement ()
     {
         Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical")).normalized;
 
         if(move != Vector3.zero)
         {
-            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
-            
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
 
-            controller.Move(move * speed * Time.deltaTime);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
-       
+    }
+   
+    void MovementTPS ()
+    {
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical")).normalized;
+
+        if(move != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y,ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+    void Jump()
+    { 
         isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
         if(isGrounded && playerVelocity.y < 0)
         {
@@ -52,3 +84,4 @@ public class Controlador : MonoBehaviour
         controller.Move(playerVelocity * Time.deltaTime);
     }
 }
+
